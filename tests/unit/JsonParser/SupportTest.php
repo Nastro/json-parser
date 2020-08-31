200bcc03a1870ced3527a46fa33a3219e1dabd27
Yury Bushenko <yury.bushenko@dalee.ru>
2020-08-31 18:55:01 +0300

    test fail

--- tests/unit/JsonParser/SupportTest.php
+++ tests/unit/JsonParser/SupportTest.php
@@ -9,7 +9,7 @@ class SupportTest extends TestCase
 {
 	public function testGetFromArraySuccess()
 	{
-		$data = ['root' => ['child' => 'test']];
+		$data = ['root' => ['child' => 'test1']];
 		$value = Support::getFromArray($data, 'root.child');
 		$this->assertSame('test', $value);
 	}
